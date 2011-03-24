809ff05f95d0cbcbf5f194d81f26326279a3e5f8
Wolfgang Kerzendorf <wkerzendorf@gmail.com>
2011-03-25 00:44:20 +1100

    added multiple things. viewIsochronces, the interpolator and other stuff

--- __init__.py
+++ __init__.py
@@ -1,7 +1,9 @@
 import os
+from iso_interpolate import *
+from iso_get import *
 
-pyIsoPath = os.path.expanduser('~/.pyisochrone')
-dbPath = os.path.join(pyIsoPath,'pyiso.db3')
+pyIsoPath = os.path.expanduser('~/.pysochrone')
+dbPath = os.path.join(pyIsoPath,'pysochrone.db3')
 #Check for the pyIsoDB3 file
 if not os.path.exists(dbPath):
     raise IOError('Database not found at %s' % dbPath)
--- iso_interpolate.py
+++ iso_interpolate.py
@@ -4,6 +4,8 @@ import numpy as np
 import sqlite3
 import pdb
 import os
+import minuit
+from matplotlib.widgets import Slider, Button, RadioButtons, CheckButtons
 
 def convertZipPickle(blob):
     return pickle.loads(bz2.decompress(blob))
@@ -13,29 +15,15 @@ def getDBConnection(dbFName = '~/.pysochrone/pysochrone.db3'):
     return sqlite3.connect(dbFName, detect_types=sqlite3.PARSE_DECLTYPES)
     
     
-def readFromDB(conn):
-
+def readFromDB(conn=None):
+    if conn==None:
+        conn = getDBConnection()
+        
     data = conn.execute('select age, mh, data from basti_iso').fetchall()
     conn.close()
     age, mh, iso = zip(*data)
-    return np.array(zip(age, mh)), np.array(iso)
-
-
-def getIsoChrone(age, mh, conn =None):
-    if conn == None:
-        conn = getDBConnection()
-    
-    points, values = readFromDB(conn)
-    crudeData = interpolate.griddata(points=points, values=values, xi = [[age,mh]])[0].reshape(2000,13)
-    return getCompleteData(crudeData)
-    
-def getMHDataDB(points, values, ageRange, mh):
-    newPoints = np.array([(item, mh) for item in ageRange])
-    newData = interpolate.griddata(points=points,
-                        values=values,
-                        xi=newPoints)
-    return newPoints, newData
-    
+    iso = np.array([item.reshape(2000,13) for item in iso])
+    return np.array(zip(age, mh)), iso
     
 def getCompleteData(data):
     fields = [('m_in',float),
@@ -51,7 +39,7 @@ def getCompleteData(data):
                 ('vk',float),
                 ('vl',float),
                 ('hk',float),
-                ('te', float),
+                ('teff', float),
                 ('mbol', float),
                 ('logg', float),
                 ]
@@ -61,6 +49,36 @@ def getCompleteData(data):
     curLen = len(te)
     newData = [tuple(item) for item in np.hstack((data, te.reshape(curLen,1), mBol.reshape(curLen,1), logg.reshape(curLen,1)))]
     return np.array(newData, dtype = fields)
+
+def getIsoInterpolator(conn=None):
+    points, values = readFromDB(conn)
+    return interpolate.LinearNDInterpolator(points, values)
+    
+def getTeffLoggInterpolator(conn=None):
+    points, values = readFromDB(conn)
+    newValues = []
+    for value in values:
+        teff = 10**value[:,3]
+        mBol = 4.75 -2.5*value[:,2]
+        logg = np.log10(value[:,1]) + 4*value[:,3] + 0.4*mBol - 12.516
+        newValues.append(np.vstack((teff, logg)))
+
+    return interpolate.LinearNDInterpolator(points, newValues)
+
+
+
+def getMinuitGrid(interpolator, params):
+    if len(params) == 0:
+        raise ValueError('Please give a kwargs')
+    def errfunc(age, feh):
+        iso = getCompleteData(interpolator((age, feh)))
+        dists = [((iso[key]-value)/(value))**2 for key, value in params.items()]
+        dist = np.sum(dists, axis=0)
+        #pdb.set_trace()
+        return np.min(dist)
+    minf = minuit.Minuit(errfunc)
+    return minf
+    
     
 def getMinDataAge(teff, logg, dataDB):
     minValues = []
@@ -86,6 +104,45 @@ def getGoodIsoAge(teff, logg, mh, isoPoints, isoValues, ageRange, ageSelectSampl
         newAgeRange = np.linspace(np.min(ageSample), np.max(ageSample), ageSampleNo)
         return getGoodIsoAge(teff, logg, mh, isoPoints, isoValues, newAgeRange, i= i+1)
     
+def showTeffLogg(teffLoggInterpolator, fig=None):
+    import pylab
+    if fig==None:
+        fig = pylab.figure(1)
+    def sliderUpdate(val):
+        teff, logg = teffLoggInterpolator(tuple([slider.val for slider in sliders]))
+        isoPlot.set_data([teff, logg])
+        plotAxis.relim()
+        plotAxis.autoscale_view()
+        pylab.draw()
+        
     
+    #adding sliders
+    sliders=[]
+    paramNames = ['age','[Fe/H]']
+    paramInit = [5, 0]
+    ages, fehs = np.unique(teffLoggInterpolator.points[:,0]), np.unique(teffLoggInterpolator.points[:,1])
+    for i,axis in enumerate([ages, fehs]):
+        ax = fig.add_axes([0.1,i*0.05+0.05,0.7,0.03])
+        sliders.append(Slider(ax, paramNames[i], min(axis), max(axis), valinit=paramInit[i]))
+        
+        
+    for slider in sliders:
+        slider.on_changed(sliderUpdate)
+        
+        
+    
+    plotAxis = fig.add_axes([0.1, 0.1+0.15, 0.8, (1-0.1+0.1)*0.7])
+#    if sampleStar != None:
+#        plotAxis.plot(sampleStar.x, sampleStar.y, lw=3, color='black')
+    teff, logg = teffLoggInterpolator(tuple([slider.val for slider in sliders]))
+    isoPlot, = plotAxis.plot(teff, logg)
+    plotAxis.invert_xaxis()
+    plotAxis.invert_yaxis()
+    plotAxis.set_xlabel('Teff [K]')
+    plotAxis.set_ylabel('log(g)')
+    pylab.show()    
+    
+
+
         
 sqlite3.register_converter("npmap", np.fromstring)
